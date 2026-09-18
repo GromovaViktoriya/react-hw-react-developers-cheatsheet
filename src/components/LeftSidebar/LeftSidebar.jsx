6310d8b635f973546874abb030e4cb7b85506e22
Viktoriya Gromova <rockingirl@mail.ru>
2026-09-18 21:36:22 +0500

    Menu fix and css

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,7 +2,7 @@ import {Layout, Menu} from "antd";
 import {menuItems} from "../../data/data.jsx";
 import {useContext} from "react";
 import ThemeContext from "../../contexts/ThemeContext/ThemeContext.js";
-import {useNavigate} from "react-router";
+import {useLocation, useNavigate} from "react-router";
 
 const {Sider} = Layout;
 
@@ -10,6 +10,9 @@ const {Sider} = Layout;
 export const LeftSidebar = () => {
     const {theme} = useContext(ThemeContext);
     const navigate = useNavigate();
+    const location = useLocation();
+
+    const currentKey = location.pathname.split('/').pop();
 
     const handleMenuClick = ({ key }) => {
         navigate(`/topics/${key}`);
@@ -23,6 +26,7 @@ export const LeftSidebar = () => {
                 theme={theme}
                 openKeys={menuItems.map(item => item.key)}
                 onClick={handleMenuClick}
+                selectedKeys={currentKey ? [currentKey] : []}
             />
         </Sider>
     )
